855bba0da339476020075a8511f146c9740f942b
ArrowNate <georgeedmon9@gmail.com>
2022-12-15 19:08:57 -0500

    g

--- SDL_Template/ScreenManager.cpp
+++ SDL_Template/ScreenManager.cpp
@@ -16,17 +16,34 @@ void ScreenManager::Release() {
 }
 
 void ScreenManager::Update() {
+	switch (mCurrentScreen) {
+	case Start:
+		m_pStartScreen->Update();
+		break;
+	case Play:
 
+		break;
+	}
 }
 
 void ScreenManager::Render() { 
-
+	switch (mCurrentScreen) {
+	case Start:
+		m_pStartScreen->Render();
+		break;
+	case Play:
+		break;
+	}
 }
 
 ScreenManager::ScreenManager() {
-	
+	m_pInput = InputManager::Instance();
+
+	mCurrentScreen = Start;
+	m_pStartScreen = new StartScreen;
 }
 
 ScreenManager::~ScreenManager() {
-	
+	m_pInput = nullptr;
+	m_pStartScreen = nullptr;
 }
--- SDL_Template/ScreenManager.h
+++ SDL_Template/ScreenManager.h
@@ -1,9 +1,16 @@
 #ifndef __SCREENMANAGER_H
 #define __SCREENMANAGER_H
+#include "StartScreen.h"
 
 class ScreenManager {
 private:
 	static ScreenManager * sInstance;
+
+	enum Screens { Start, Play };
+	Screens mCurrentScreen;
+
+	InputManager* m_pInput;
+	StartScreen* m_pStartScreen;
 
 public:
 	static ScreenManager * Instance();
